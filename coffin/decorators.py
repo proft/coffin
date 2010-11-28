385e4dc1b7addfc3a5db6fdf58986d38300ee74c
proft <proft@ua.fm>
2010-11-28 22:29:15 +0200

    added @render_to decorator

---
+++ coffin/decorators.py
@@ -0,0 +1,29 @@
+from coffin.shortcuts import render_to_response
+from coffin.template import RequestContext
+
+try:
+    from functools import wraps
+except ImportError: 
+    def wraps(wrapped, assigned=('__module__', '__name__', '__doc__'),
+              updated=('__dict__',)):
+        def inner(wrapper):
+            for attr in assigned:
+                setattr(wrapper, attr, getattr(wrapped, attr))
+            for attr in updated:
+                getattr(wrapper, attr).update(getattr(wrapped, attr, {}))
+            return wrapper
+        return inner
+
+def render_to(template=None, mimetype="text/html"):
+    def renderer(function):
+        @wraps(function)
+        def wrapper(request, *args, **kwargs):
+            output = function(request, *args, **kwargs)
+            if not isinstance(output, dict):
+                return output
+            tmpl = output.pop('TEMPLATE', template)
+            return render_to_response(tmpl, output, \
+                        context_instance=RequestContext(request), mimetype=mimetype)
+        return wrapper
+    return renderer
+
